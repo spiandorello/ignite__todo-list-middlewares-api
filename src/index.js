bba6b216ac4a1bb4494ab3d3b39c2b86635bb959
Eduardo Da Veiga Spiandorello <eduardo.spiandorello@gmail.com>
2021-03-13 17:10:37 -0300

    Add validation middlewars

--- src/index.js
+++ src/index.js
@@ -10,19 +10,81 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({
+      error: 'Username not found!',
+    });
+  }
+  
+  request.user = user;
+  
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({
+      error: 'Cannot create a new todo on free plan!', 
+    });
+  }
+  
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({
+      error: 'Username not found!',
+    });
+  }
+  
+  if (!validate(id)) {
+    return response.status(400).json({
+      error: 'The id must be an uuid',
+    });
+  }
+  
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({
+      error: 'Todo not found',
+    });
+  }
+  
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({
+      error: 'The id must be an uuid',
+    });
+  }
+
+  const user = users.find(user => user.id === id);
+  if (!user) {
+    return response.status(404).json({
+      error: 'User not found',
+    });
+  }
+  
+  request.user = user;
+  
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -91,7 +153,7 @@ app.post('/todos', checksExistsUserAccount, checksCreateTodosUserAvailability, (
 app.put('/todos/:id', checksTodoExists, (request, response) => {
   const { title, deadline } = request.body;
   const { todo } = request;
-
+  
   todo.title = title;
   todo.deadline = new Date(deadline);
 
@@ -106,7 +168,7 @@ app.patch('/todos/:id/done', checksTodoExists, (request, response) => {
   return response.json(todo);
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, checksTodoExists, (request, response) => {
+app.delete('/todos/:id', checksTodoExists, (request, response) => {
   const { user, todo } = request;
 
   const todoIndex = user.todos.indexOf(todo);
